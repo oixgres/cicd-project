6865ff43d5131965ec18c030ca916fcd2a3f4b8a
Daniela <118323771+dannie234234rvfs@users.noreply.github.com>
2025-05-08 15:06:21 -0700

    updateee

--- app.py
+++ app.py
@@ -4,5 +4,6 @@ while True:
     sleep(2)
     print("Testing new pr using default namespace... edit Carmona")
     print("Testing new vivian...")
+    print("Hola Galgos")
     print("Testing new pr using default hola mundo...")
     print("Testing new usuario sergio...")
