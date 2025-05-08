dad742f157c8b4e1739aa129af30f16724765823
SergioRivera8888S <sarv2405@gmail.com>
2025-05-08 14:46:15 -0700

    Update app.py

--- app.py
+++ app.py
@@ -3,5 +3,5 @@ from time import sleep
 while True:
     sleep(10)
     print("Testing new pr using default namespace...")
-    print("Testing new pr using defaulpace...")
-m
+    print("Testing new usuario sergio...")
+
